6d542eda988c8b2ab16e5d2bf0656ba380808541
Kouichi Ikeda <Kouichi@Kouichi-Ikeda-no-iMac.local>
2016-10-10 00:56:53 +0900

    PORTFOLIO TOP 初回コミット

---
+++ js/jquery.scroll.js
@@ -0,0 +1,11 @@
+$( function() {
+  $( "a[href^=#]" ).click( function() {
+    var href     = $( this ).attr( "href" );
+    var target   = $( href == "#" || href == "" ? "html" : href );
+    var position = target.offset().top;
+
+    $( "body,html" ).animate( { scrollTop: position }, 400, "swing" );
+
+    return false;
+  } );
+} );
