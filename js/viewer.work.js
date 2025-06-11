c22e8e6b813eb9049127daaff3348d4e8fbe1b81
池田 晃一 <>
2025-06-11 23:45:22 +0900

    PORTFOLIO Viewer実装

---
+++ js/viewer.work.js
@@ -0,0 +1,9 @@
+const options = {
+    button: false,
+    navbar: false,
+    rotatable: false,
+    title: false,
+    toolbar: false
+};
+
+new Viewer(document.getElementById( "mockup" ), options);
